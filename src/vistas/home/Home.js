dd9fa90701f74cb1fc4de6e336d063b9d944db98
Gabelonio <gabelonio@hotmail.com>
2022-07-04 11:09:21 -0500

    solucionado problema de llamado de paquetes

--- src/vistas/home/Home.js
+++ src/vistas/home/Home.js
@@ -9,21 +9,14 @@ export default function Home(){
       }, []);
 
     return(
-/*         <div> */
-            <div className="contenedor" data-aos = "fade-right" data-aos-once="true">
-                <div className="text" >
-                    <h1>Una nueva manera de conectar con alumnos</h1>
-                    <button className='bt_home'>Unete</button>
-                </div>
-                <div  className="imagen">
-                <img alt='Img de presenatcion' src='https://www.pandasecurity.com/es/mediacenter/src/uploads/2020/07/pandasecurity-juegos-online.jpg'/>
-                </div>
+        <div className="contenedor" data-aos = "fade-right" data-aos-once="true">
+            <div className="text" >
+                <h1>Una nueva manera de conectar con alumnos</h1>
+                <button className='bt_home'>Unete</button>
             </div>
-/*             <div>
-                <SaberMas/>
-                <Testimonios/>
-                <Preguntas/>
+            <div  className="imagen">
+            <img alt='Img de presenatcion' src='https://www.pandasecurity.com/es/mediacenter/src/uploads/2020/07/pandasecurity-juegos-online.jpg'/>
             </div>
-        </div> */
+        </div>
     )
 }
