0ea216409aca3c43e89fb604bda5ab1b8c1b164c
Gabelonio <gabelonio@hotmail.com>
2022-07-03 22:33:17 -0500

    colores, fuentes y animaciones agregados

--- src/vistas/Home.js
+++ src/vistas/Home.js
@@ -3,8 +3,8 @@ import './home.css';
 export default function Home(){
 
     return(
-        <div className="contenedor">
-            <div className="text">
+        <div className="contenedor" data-aos = "fade-right">
+            <div className="text" >
                 <h1>Una nueva manera de conectar con alumnos</h1>
                 <button className='bt_home'>Unete</button>
             </div>
--- src/vistas/Preguntas.js
+++ src/vistas/Preguntas.js
@@ -1,13 +1,19 @@
 import './preguntas.css';
 import Pregunta from '../componentes/pregunta/Pregunta';
 import data from '../data_prueba/faq.json';
+import { useEffect } from 'react';
+import Aos from 'aos';
 
 
 export default function Preguntas(props) {
 
+    useEffect(() => {
+        Aos.init({duration : 1000});
+      }, []);
+    
     return(
-        <div>
-            <div className="pr_title">
+        <div data-aos = "fade-in">
+            <div className="pr_title" >
                 <h1>FAQ</h1>
                 <p>¿Tienes alguna duda? Aquí se encuentran las preguntas más 
                 recurrentes que puedes tener</p>
