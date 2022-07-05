65a56e14277adb777579e3810ce0e3f5c6e9eb1c
jordy Pineda <jepinedav@gmail.com>
2022-07-04 20:09:04 -0500

    menu izq Estudiantes

--- src/vistas/estudiante/ClasesEst.js
+++ src/vistas/estudiante/ClasesEst.js
@@ -1,11 +1,13 @@
+import Ficha from '../../componentes/ficha/Ficha';
 import './clasesEst.css';
 
 
 export default function ClasesEst(props){
 
+    const data=['Matematicas 1', "id_ de la clase?"]
     return( <div>
                <div className="misClases">
-                
+                    <Ficha/>
                </div>
 
                <div className="ingresarClase">buscar clase</div>
